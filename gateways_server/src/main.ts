c1ba234c208c04669e51fae2ce9d761ccb8bc653
An2 <an2m001@gmail.com>
2022-11-20 23:04:30 -0500

    class-transformer added an main.ts ready to go!

--- gateways_server/src/main.ts
+++ gateways_server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) => new BadRequestException(errors),
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
