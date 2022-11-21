a9b876f1e86e2fed83700d88f6ec209d36022651
An2 <an2m001@gmail.com>
2022-11-21 14:02:32 -0500

    Logo gateway .gif and fav icon created

---
+++ gateways_ui/pages/device.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout';
+
+import styles from '../styles/Home.module.css';
+
+export default function Device() {
+  return (
+    <div className={styles.container}>
+      <Layout></Layout>
+    </div>
+  );
+}
