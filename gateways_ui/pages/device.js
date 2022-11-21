0229d6d6a71a279ac413d6de5a1679abf8e1b703
An2 <an2m001@gmail.com>
2022-11-21 14:22:54 -0500

    bootstrap added

--- gateways_ui/pages/device.js
+++ gateways_ui/pages/device.js
@@ -1,11 +1,3 @@
-import Layout from '../components/layout';
-
-import styles from '../styles/Home.module.css';
-
 export default function Device() {
-  return (
-    <div className={styles.container}>
-      <Layout></Layout>
-    </div>
-  );
+  return <>Device route</>;
 }
