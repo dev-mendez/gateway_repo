ca5462bfe2fd345dedf3137e0e8f2c6582f8704e
An2 <an2m001@gmail.com>
2022-11-27 01:12:53 -0500

    adding enviroment var

--- gateways_server/src/main.ts
+++ gateways_server/src/main.ts
@@ -2,8 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
+  console.log(process.env.name);
+
   const app = await NestFactory.create(AppModule, { cors: true });
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(
