7a88eb1254984f1cc5cd8b0cd786ca126bd87bca
An2 <an2m001@gmail.com>
2022-11-27 00:38:42 -0500

    Styles added to the footer and body.

--- gateways_ui/components/gateway/add-gateway-form.js
+++ gateways_ui/components/gateway/add-gateway-form.js
@@ -18,7 +18,7 @@ const style = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
-  bgcolor: '#fcfcfc',
+  bgcolor: 'rgb(234, 243, 253)',
   border: '1px solid #000',
   boxShadow: 24,
   p: 4,
