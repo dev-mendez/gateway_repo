a1ea2da05b8b387cc4ff8934a6d1bfc3bcbb59d1
An2 <an2m001@gmail.com>
2022-11-27 22:00:29 -0500

    Added env var to the server

--- gateways_server/src/app.module.ts
+++ gateways_server/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GatewayModule } from './gateways/gateway.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import env from './config/env-config';
 
 @Module({
-  imports: [
-    GatewayModule,
-    MongooseModule.forRoot('mongodb://localhost/gateway_app'),
-  ],
+  imports: [GatewayModule, MongooseModule.forRoot(env.MONGODB_URI)],
   controllers: [],
   providers: [],
 })
--- gateways_server/src/main.ts
+++ gateways_server/src/main.ts
@@ -2,13 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import env from './config/env-config';
 
 async function bootstrap() {
-  console.log(process.env.name);
-
   const app = await NestFactory.create(AppModule, { cors: true });
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(
@@ -16,6 +12,6 @@ async function bootstrap() {
       exceptionFactory: (errors) => new BadRequestException(errors),
     }),
   );
-  await app.listen(3000);
+  await app.listen(env.API_PORT);
 }
 bootstrap();
